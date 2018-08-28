587d51c3356308ef7e3fbf92c0e294c08baebf73
SorMV <cur2008@mail.ru>
2018-08-28 22:54:12 +0300

    modified PyramidBuilder and exception classes

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -1,5 +1,7 @@
 package com.tsystems.javaschool.tasks.pyramid;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class PyramidBuilder {
@@ -13,9 +15,42 @@ public class PyramidBuilder {
      * @throws {@link CannotBuildPyramidException} if the pyramid cannot be build with given input
      */
     public int[][] buildPyramid(List<Integer> inputNumbers) {
-        // TODO : Implement your solution here
-        return new int[0][0];
+        int n = isPyramid(inputNumbers.size());
+        if (n != 0) {
+            int[][] mas = new int[n][2 * n - 1];
+            Collections.sort(inputNumbers);
+            int indx = 0;
+            for (int i = 0; i < n; i++) {
+                int k = n - i - 1;
+                for (int j = 0; j <= i; j++) {
+                    mas[i][k] = inputNumbers.get(indx);
+                    indx += 1;
+                    k += 2;
+                }
+            }
+            return mas;
+        }
+        throw new CannotBuildPyramidException("Can't build pyramid from this list of numbers");
     }
 
+    private int isPyramid(int size) {
+        if (size == 0) return 0;
+        for (int i = 1; i <= size / 2 + 1; i++) {
+            if (size == i * (i + 1) / 2) {
+                return i;
+            }
+        }
+        return 0;
+    }
 
+    public static void main(String[] args) {
+        PyramidBuilder pb = new PyramidBuilder();
+        int[][] mas = pb.buildPyramid(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15));
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(mas[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
