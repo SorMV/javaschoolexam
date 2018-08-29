cd07743cd2304aa4a936b9d9c17ac2e23e9c9be4
SorMV <33803127+SorMV@users.noreply.github.com>
2018-08-29 18:52:37 +0300

    Update SubsequenceImpl.java

--- src/main/java/com/tsystems/javaschool/tasks/subsequence/SubsequenceImpl.java
+++ src/main/java/com/tsystems/javaschool/tasks/subsequence/SubsequenceImpl.java
@@ -3,18 +3,16 @@ package com.tsystems.javaschool.tasks.subsequence;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class SubsequenceImpl extends Subsequence {
     public boolean find(List subSeq, List seq) {
-        if (seq != null && subSeq != null) {
-            if (subSeq.size() == 0) {
-                return true;
-            }
-              else {
+
             if (subSeq.size() > seq.size()) {
                 return false;
             }
-
             int i = 0;
             for (int j = 0; j < seq.size(); j++) {
                 if (subSeq.get(i).equals(seq.get(j))) {
@@ -23,18 +21,7 @@ public class SubsequenceImpl extends Subsequence {
                     } else return true;
                 }
             }
-        }
-        } else {
-            throw new IllegalArgumentException("One or both sequences have no elements");
-        }
         return false;
     }
-
-    public static void main(String[] args) {
-        SubsequenceImpl s = new SubsequenceImpl();
-        List x = new ArrayList();
-        List y = new ArrayList();
-        boolean b = s.find(x,y);
-        System.out.println(b); // Result: true
-       }
+    
 }
