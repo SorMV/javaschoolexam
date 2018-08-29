4a10941937e5a9ff732c58d0e55ddefd3b85cf8e
SorMV <33803127+SorMV@users.noreply.github.com>
2018-08-29 18:05:36 +0300

    add subseq.size==0 case

--- src/main/java/com/tsystems/javaschool/tasks/subsequence/SubsequenceImpl.java
+++ src/main/java/com/tsystems/javaschool/tasks/subsequence/SubsequenceImpl.java
@@ -1,17 +1,21 @@
 package com.tsystems.javaschool.tasks.subsequence;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class SubsequenceImpl extends Subsequence {
     public boolean find(List subSeq, List seq) {
         if (seq != null && subSeq != null) {
-            if (subSeq.size() > 0 && seq.size() > 0) {
+            if(subSeq.size()==0) {
+                return true;
+            }
                 if (subSeq.size() > seq.size()) {
                     System.out.println("Second seq. must be longer than first.");
                     return false;
                 }
-        } else throw new IllegalArgumentException("One or both sequences have no elements");
+        
+        
             int i = 0;
             for (int j = 0; j < seq.size(); j++) {
                 if (subSeq.get(i).equals(seq.get(j))) {
@@ -28,8 +32,9 @@ public class SubsequenceImpl extends Subsequence {
 
     public static void main(String[] args) {
         SubsequenceImpl s = new SubsequenceImpl();
-        boolean b = s.find(Arrays.asList("A", "B", "C", "D", "H"),
-                Arrays.asList("BD", "A", "ABC", "B", "M", "D", "M", "C", "DC", "H", "C", "D"));
+        List x = new ArrayList();
+        List y = new ArrayList();
+        boolean b = s.find(x,y);
         System.out.println(b); // Result: true
     }
 }
